213b317e5e6bcab11ac9e8245b9ab5b9581c1cda
Waheed Khaled <waheedkhaled95@gmail.com>
2024-05-02 22:55:35 +0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,13 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
