4156a665ac23b1bbaa7da68353bfcc66bb14c454
Waheed Khaled <waheedkhaled95@gmail.com>
2024-05-02 12:37:09 +0300

    Test Task 03

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,9 @@
 File that contains Blueprint
 """
 from flask import Blueprint
-from api.v1.views.index import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+from api.v1.views.index import *
