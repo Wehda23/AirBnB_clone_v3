42c4b9442effba8e5f71ac900e4a3756f6418316
Waheed Khaled <waheedkhaled95@gmail.com>
2024-05-02 13:33:12 +0300

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """
 Contains apis
 """
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -11,3 +18,19 @@ def OKStatus():
     """Function to return ok status"""
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route("/stats")
+def stats():
+    """Function to return stats"""
+    models = {
+        "states": State,
+        "cities": City,
+        "amenities": Amenity,
+        "places": Place,
+        "reviews": Review,
+        "users": User
+    }
+    for key in models:
+        models[key] = storage.count(models[key])
+    return jsonify(models)
+
